e9a33313baf459bab3626f92f2f6340d070e598a
Ines-karega <i.karega@alustudent.com>
2026-06-17 15:05:13 +0200

    Replace f-strings with .format() in number route for Python 3.5 compatibility

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def python_route(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     """Return n is a number only if n is an integer."""
-    return f'{n} is a number'
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
